d574ede780ea9b5cb707622576fd584322a5e8c5
Theodoros Malezidis <tmalezidis@gmail.com>
2025-04-06 18:55:43 +0300

    Update rankings.sql

--- Reports/Basic Report (EDA)/rankings.sql
+++ Reports/Basic Report (EDA)/rankings.sql
@@ -1,1 +1,133 @@
+/*Top and worst performers across key areas highlighting leaders and trends.
+*/
+
+
+
+
+-- HOLDINGS
+
+-- Top 10 holdings by total investment value
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY SUM(invested_amount) DESC) AS holdings_ranking,
+	p.product_name,
+	p.product_type,
+	SUM(invested_amount) AS total_investment_value
+FROM
+	gold.dim_products p
+LEFT JOIN gold.fact_transactions f
+ON p.product_key=f.product_key
+GROUP BY
+	p.product_name,
+	p.product_type) r
+WHERE
+	holdings_ranking <= 10
+ORDER BY
+	holdings_ranking;
+
+
+-- Top 10 types of holdings by total AUM
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY SUM(invested_amount)-SUM(withdrawal_amount) DESC) AS holdings_ranking,
+	p.product_name,
+	p.product_type,
+	SUM(invested_amount)-SUM(withdrawal_amount) AS total_AUM
+FROM
+	gold.dim_products p
+LEFT JOIN gold.fact_transactions f
+ON p.product_key=f.product_key
+GROUP BY
+	p.product_name,
+	p.product_type) r
+WHERE
+	holdings_ranking <= 10
+ORDER BY
+	holdings_ranking;
+
+
+-- CLIENTS
+
+--Top 10 Clients by AUM
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY SUM(invested_amount)-SUM(withdrawal_amount) DESC) AS clients_ranking,
+	c.client_full_name,
+	SUM(invested_amount)-SUM(withdrawal_amount) AS AUM
+FROM
+	gold.dim_clients c
+LEFT JOIN gold.fact_transactions t
+ON c.client_key=t.client_key
+GROUP BY
+	c.client_full_name) r
+WHERE
+	clients_ranking <= 10
+ORDER BY
+	clients_ranking;
+
+-- Top 10 Clients by No. of Investments Placed
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY Count(invested_amount) DESC) AS clients_ranking,
+	c.client_full_name,
+	COUNT(invested_amount) AS investments_count
+FROM
+	gold.dim_clients c
+LEFT JOIN gold.fact_transactions t
+ON c.client_key=t.client_key
+GROUP BY
+	c.client_full_name) r
+WHERE
+	clients_ranking <= 10
+ORDER BY
+	clients_ranking;
+
+-- EMPLOYEES
+
+--Top 10 employees by number of active portfolios managed
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY COUNT (c.client_key) DESC) AS employees_ranking,
+	e.employee_full_name,
+	COUNT (c.client_key) AS active_portfolios
+FROM
+	gold.dim_employees e
+LEFT JOIN gold.fact_employee_client f
+ON e.employee_key=f.employee_key
+LEFT JOIN gold.dim_clients c
+ON f.client_key=c.client_key
+WHERE
+	c.create_date IS NOT NULL
+GROUP BY
+	e.employee_full_name) r
+WHERE
+	employees_ranking <= 10
+ORDER BY
+	employees_ranking;
+
+--Top 10 employees by number of investment holdings analyzed.
+SELECT *
+FROM(
+SELECT
+	ROW_NUMBER() OVER(ORDER BY COUNT (p.product_key) DESC) AS employees_ranking,
+	e.employee_full_name,
+	COUNT (p.product_key) AS holdings_analyzed
+FROM
+	gold.dim_employees e
+LEFT JOIN gold.fact_employee_product f
+ON e.employee_key=f.employee_key
+LEFT JOIN gold.dim_products p
+ON f.product_key=p.product_key
+GROUP BY
+	e.employee_full_name) r
+WHERE
+	employees_ranking <= 10
+ORDER BY
+	employees_ranking;
+
 
